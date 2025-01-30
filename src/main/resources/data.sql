df31dce6185fb48fc8d6a991baec4acf095b02ce
Vincent Caron <caron.vincent@protonmail.com>
2025-01-30 14:35:33 +0100

    feat: replicate base DB structure

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO task_categories VALUES (default, 'FirstCat', 'Description of FirstCat');
+
+INSERT INTO tasks VALUES (default, 'FirstTask', 'Description of first task', '2000-01-01', 1);
+INSERT INTO tasks VALUES (default, 'task2', 'Description of second task', '2000-01-01', 1);
